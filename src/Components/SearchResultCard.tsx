2ef929cc4033878c13748d5f35f4a4b602605bb4
Robert Mang <Robert.Mang@softgarden.de>
2023-11-27 01:53:27 +0100

    remove button to open links for unused urls

--- src/Components/SearchResultCard.tsx
+++ src/Components/SearchResultCard.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {FaPlay} from 'react-icons/fa'
+import { FaPlay } from 'react-icons/fa'
 type SearchResultCardProperties = {
-    Url: string
-    IsAvailable: boolean
+  Url: string
+  IsAvailable: boolean
 }
 
 type SearchResultCardState = {
@@ -10,29 +10,31 @@ type SearchResultCardState = {
 
 
 class SearchResultCard extends React.Component<SearchResultCardProperties, SearchResultCardState>{
-    constructor(props: SearchResultCardProperties) {
-        super(props);
-    }
+  constructor(props: SearchResultCardProperties) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="bg-yellow-500 min-h-0 h-20 mt-1 mb-1 mx-60 w-90 flex items-center justify-between rounded-md">
-                <div className="ml-5 font-bold">
-                    {this.props.Url}
-                </div>
-                {/* {this.props.IsAvailable ?
-                    <div className="mr-5">
-                        <img src="404image" />
-                    </div> 
-                    : */}
-                    <div className="mr-5">
-                        <a href={this.props.Url} target="_blank"><FaPlay /></a>
-                    </div>
-                {/* } */}
-                
+  render() {
+    return (
+      <div className="bg-yellow-500 min-h-0 h-20 mt-1 mb-1 mx-60 w-90 flex items-center justify-between rounded-md">
+        <div className="ml-5 font-bold">
+          {this.props.Url}
+        </div>
+        {
+          !this.props.IsAvailable ? (
+            <div className="mr-5">
             </div>
-        )
-    }
+          ) : (
+            <div className="mr-5">
+              <a href={this.props.Url} target="_blank" rel="noopener noreferrer">
+                <FaPlay />
+              </a>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default SearchResultCard
